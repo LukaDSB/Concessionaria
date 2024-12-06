a7371f950893291e97b72fbf36448cae7451c3ff
Lucas Benevenute <souzalucas6@hotmail.com>
2024-12-06 14:03:11 -0300

    Começo do header

--- components/headerComponent.php
+++ components/headerComponent.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/principalStyles.scss">
+    <script src="https://kit.fontawesome.com/0417d8a52d.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="headerTitle">
@@ -12,8 +13,23 @@
 
     <div class="headerNavbar">
         <ul>
-            <li></li>
-            <li></li>
+            <li><img src="../styles/logo.avif" alt=""></li>
+            <li><button>Zero Km</button></li>
+            <li><button>Seminovos</button></li>
+            <li><button>Tabela FIPE</button></li>
+            <li><button>Serviços e peças</button></li>
+            <li><button>Financiamento</button></li>
+            <li><button>Vendas diretas</button></li>
+            <li><button>Lojas</button></li>
+            <li>
+                <button><i class="fa-solid fa-magnifying-glass"></i></i>Buscar</button>
+            </li>
+            <li>
+                <button><i class="fa-regular fa-user"></i>Entrar</button>
+            </li>
+            <li>
+                <button><i class="fa-regular fa-heart"></i>Favoritos</button>
+            </li>
         </ul>
     </div>
 </body>
--- views/principal.php
+++ views/principal.php
@@ -1,1 +1,13 @@
-<?php include '../components/headerComponent.php';?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <script src="https://kit.fontawesome.com/0417d8a52d.js" crossorigin="anonymous"></script>
+    </head>
+    <body>  
+        
+        <?php include '../components/headerComponent.php';?>
+    </body>
+</html>
