c3048fc2ee49681831e0ae8a887ee0d567477432
Mori <moriartyvf@gmail.com>
2025-01-05 15:13:15 -0300

    Att

--- components/cardCarouselComponent.php
+++ components/cardCarouselComponent.php
@@ -6,40 +6,15 @@
     <title>Document</title>
     <link rel="stylesheet" href="../styles/cardCarousel.css">
     <style>@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css");</style>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
+    
 </head>
 <body>
 
-
-<div class="swiper">
-  <!-- Additional required wrapper -->
-  <div class="swiper-wrapper">
-    <!-- Slides -->
-    <div class="swiper-slide">Slide 1</div>
-    <div class="swiper-slide">Slide 2</div>
-    <div class="swiper-slide">Slide 3</div>
-    ...
-  </div>
-  <!-- If we need pagination -->
-  <div class="swiper-pagination"></div>
-
-  <!-- If we need navigation buttons -->
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
-
-  <!-- If we need scrollbar -->
-  <div class="swiper-scrollbar"></div>
-</div>
-
-
-<!--  -->
-
-    <div class="card">
-        <!-- carousel -->
+<!-- CARD -->
+        <div class="card">
+            <img src="#" alt="FotoCarro">
         <div class="logo-e-btn">
-            <img src="" alt="Logo">
+            <img src="#" alt="Logo">
             <a href="#">Ver parcelas</a>
         </div>
         <div class="titulo">
@@ -56,7 +31,6 @@
         <i class="bi bi-geo-alt-fill"></i>
         <p>Disponível em n lojas</p>
         </div>
-
     </div>
 </body>
 </html>
